3db8831e1dea038c0f6554e90c8158e4653788c8
Praveen <spraveen593@gmail.com>
2022-02-24 18:50:23 +0530

    Fixed: Vector sort

--- partA/VectorDemo.java
+++ partA/VectorDemo.java
@@ -8,18 +8,19 @@ public class VectorDemo {
     }
 
     public static void main(String args[]) {
-        Vector list = new Vector();
+        Vector v = new Vector();
         int length = args.length;
         for (int i = 0; i < length; i++) {
-            list.addElement(args[i]);
+            v.addElement(args[i]);
         }
-        print("Vector Element before Soring");
-        for (int i = 0; i < list.size(); i++) {
-            print("" + list.get(i));
+        print("Vector elems before sort");
+        for (int i = 0; i < length; i++) {
+            print("" + v.get(i));
         }
-        print("Vector Elements after Sorting");
-        for (int i = 0; i < list.size(); i++) {
-            print("" + list.get(i));
+        Collections.sort(v);
+        print("Vector elems after sort");
+        for (int i = 0; i < length; i++) {
+            print("" + v.get(i));
         }
     }
 }
